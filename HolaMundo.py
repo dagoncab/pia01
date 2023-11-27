965f806d4e6177afccf67c53c014b91d5812a442
dagoncab <152216285+dagoncab@users.noreply.github.com>
2023-11-27 22:14:04 +0100

    Create HolaMundo.py
    Creacion del archivo holamundo.py

---
+++ HolaMundo.py
@@ -0,0 +1,7 @@
+# Hola mundo en Python
+
+# Se podria crear una variable donde almacenar el saludo, por si en un futuro se decide cambiar o personalizar.
+print "Hola mundo!"
+# Ejemplo de lo anterior.
+nombre = raw_input("Dime como te llamas: ")
+print "Homa mundo! de ", nombre
